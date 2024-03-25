553b0a5a3b076e6f88cf127f92cf7f8d0634ab78
NikolaSiljanovski <siljanovski0@gmail.com>
2024-03-25 10:20:09 +0100

    Added changes.java

--- src/main/java/Devv.java
+++ src/main/java/Devv.java
@@ -1,8 +1,31 @@
+import java.util.Scanner;
 
 public class Devv {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+
+		int i, n;
+
+		System.out.println("Do brojot: ");
+		n = input.nextInt();
+
+		for (i = 1; i <= n; i++) {
+
+			if (i % 5 != 0) {
+				System.out.println(i + " " + "Ne e delliv so 5");
+			}
+
+		}
+		System.out.println("---------------------");
+		for (i = 1; i <= n; i++) {
+
+			if (i % 5 == 0) {
+				System.out.println(i + " " + "E delliv so 5 ");
+			}
+
+		}
 
 	}
 
